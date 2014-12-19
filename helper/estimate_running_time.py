e421f2524fb17323a297add26fcb5a2077c024ab
Akul <akurnya@gmail.com>
2014-12-19 16:15:06 +0530

    added a rough n easy time estimation module

---
+++ helper/estimate_running_time.py
@@ -0,0 +1,26 @@
+"""Debugging helper.
+Fast and easy way to Roughly gets the running time of a function.
+Implement a counter in the original function in loops.
+Make sure to include counter var in the i/p and return the counter value in the called func as well."""
+
+from math import log
+from matplotlib import pyplot as plt
+
+
+def running_time(func, counter, plot_type, *args, **kwargs):
+    """
+    A very simple debug to test loops or no loops running time.
+    func: some math function thta returns a numeric value
+    plot_type: can be LOGLOG or Standard
+    Note:
+    Have to add counter to the func function as well
+    """
+    plots = []
+    counter = 0
+    #include counter
+    output = (func(counter, *args, **kwargs))  #returns [value, counter]
+    if plot_type == "standard".upper():
+        plots.append(output)
+    else:
+        plots.append((log(value), log(counter)))
+    return plt.plot(plots)
