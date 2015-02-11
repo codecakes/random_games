698ced79950beafd12f6f05e30085ba2a5ade298
Akul <akurnya@gmail.com>
2015-02-12 01:57:13 +0530

    added kramers strength relationship score and comments

--- probability_combinatorics/chi2_stat.py
+++ probability_combinatorics/chi2_stat.py
@@ -1,6 +1,10 @@
 from pandas import DataFrame
 from scipy.stats import chisqprob
 
+"""
+Chi2 Analysis should be used with Independent Test Subjects
+"""
+
 def chi2(original,expected):
     """
     Given 2 dataframes:
@@ -10,9 +14,18 @@ def chi2(original,expected):
     """
     return ((original-expected)**2/expected).values.sum()
 
+
 def chi2_cmp(chi2_val, df, alpha_val):
     """
     if the probability of getting chi2_val at df is < alpha_val is TRUE,
     then it is a significant val. Reject the Null.
     """
     return round(chisqprob(chi2_val, df), 4) < alpha_val
+
+def kramers_phi_v(chi2_val, num_samples, d):
+    """
+    used when > 2x2 matrix;
+    testing for strength between the two variables
+    """
+    k = min((d.columns.size,d.index.size))
+    return sqrt(float(chi2_val)/(num_samples*(k-1)))
