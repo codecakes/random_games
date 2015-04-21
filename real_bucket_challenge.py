958311dc8f69ba97e7008f8e266b32dc7573b2ce
Akul <akurnya@gmail.com>
2015-04-21 22:18:42 +0530

    Works with examples

--- real_bucket_challenge.py
+++ real_bucket_challenge.py
@@ -19,34 +19,36 @@ from collections import deque
 def fill(container):
     if len(container) < container.maxlen:
         diff = container.maxlen - len(container)
-        [container.append(1) for _ in xrange(diff)]
-        print "after filling container %s" %(container)
+        #[container.append(1) for _ in xrange(diff)]
+        container.extend([1]*diff)
+        #print "after filling container %s" %(container)
 
 def pour(cn1, cn2):
     if len(cn1) and (len(cn2) < cn2.maxlen) and cn1.maxlen < cn2.maxlen:
         diff = cn2.maxlen - len(cn2)
         [cn2.append(cn1.pop()) for _ in xrange(diff) if cn1]
-        print "after pouring %s and %s" %(cn1, cn2)
+        #print "after pouring %s and %s" %(cn1, cn2)
 
 def empty(container):
     container.clear()
-    print "container clear"
+    #print "container clear"
     
 def fill_buck(small, large, target_val):
+    count = 0
     large_val = large.maxlen
-    print "target val %s large val %s" %(target_val, large_val)
+    #print "target val %s large val %s" %(target_val, large_val)
     if target_val < large_val:
         while len(large)!= target_val:
-            print "target val %s large %s" %(target_val, large)
+            count += 1
+            #print "target val %s large %s" %(target_val, large)
             fill(small)
             pour(small, large)
-            #if len(large) == large_val:
-            #    empty(large)
             if len(small):
                 #if small not empty?
                 if len(large) == large_val:
                     empty(large)
                 pour(small, large)
+            print "%s times" %(count)
 
 def run_bucks(small_max, large_max, target_val):
     small = deque(maxlen=small_max)
