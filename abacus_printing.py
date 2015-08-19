e05b00c2421559f3c3042b458d9ad80d2d284de5
Akul <akurnya@gmail.com>
2015-08-19 20:18:47 +0530

    more solns!

---
+++ abacus_printing.py
@@ -0,0 +1,101 @@
+#########################################################################
+#                 10-row School abacus
+#                         by
+#                      Michael H
+#########################################################################
+#       Description partially extracted from from wikipedia 
+#
+#  Around the world, abaci have been used in pre-schools and elementary
+#
+# In Western countries, a bead frame similar to the Russian abacus but
+# with straight wires and a vertical frame has been common (see image).
+# Helps schools as an aid in teaching the numeral system and arithmetic
+#
+#         |00000*****   |     row factor 1000000000
+#         |00000*****   |     row factor 100000000
+#         |00000*****   |     row factor 10000000 
+#         |00000*****   |     row factor 1000000
+#         |00000*****   |     row factor 100000
+#         |00000*****   |     row factor 10000
+#         |00000*****   |     row factor 1000
+#         |00000****   *|     row factor 100     * 1
+#         |00000***   **|     row factor 10      * 2
+#         |00000**   ***|     row factor 1       * 3
+#                                        -----------    
+#                             Sum                123 
+#
+# Each row represents a different row factor, starting with x1 at the
+# bottom, ascending up to x1000000000 at the top row.     
+######################################################################
+
+# TASK:
+# Define a procedure print_abacus(integer) that takes a positive integer
+# and prints a visual representation (image) of an abacus setup for a 
+# given positive integer value.
+# 
+# Ranking
+# 1 STAR: solved the problem!
+# 2 STARS: 6 < lines <= 9
+# 3 STARS: 3 < lines <= 6
+# 4 STARS: 0 < lines <= 3
+import math
+
+def print_abacus(value):
+    ### Add you code here 
+    #
+    #take base num num = 10 and take mod rem of string s
+    # prefix string between | and |, where string s is,
+    # s='00000*****' => s[:-1*mod_rem] + ' '*3 + s[-1*mod_rem:] => "|<s>|\n
+    # multiple num *= 10
+    #until the last result is same in next iteration, then break
+    num = 10
+    val = value
+    s = '00000*****'
+    last = None
+    res = ''
+    while last != value:
+        val = (value%num)
+        mod_rem = val/(num/10)
+        res = '|' + (s[:-1*mod_rem] + ' '*3 + s[-1*mod_rem:] if mod_rem > 0 else s + ' '*3) + '|\n' + res
+        if val != last: last = val
+        num *= 10
+    print (("|" + s + ' '*3 + "|\n") * int(math.log10((10**10)//num)))+res
+    
+
+###  TEST CASES
+print "Abacus showing 0:"
+print_abacus(0)
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+print "Abacus showing 12345678:"
+print_abacus(12345678)
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000****   *|
+#>>>|00000***   **|
+#>>>|00000**   ***|
+#>>>|00000*   ****|
+#>>>|00000   *****|
+#>>>|0000   0*****|
+#>>>|000   00*****|
+#>>>|00   000*****|
+print "Abacus showing 1337:"
+print_abacus(1337)
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000*****   |
+#>>>|00000****   *|
+#>>>|00000**   ***|
+#>>>|00000**   ***|
+#>>>|000   00*****|
