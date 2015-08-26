e12e4a62e7acd7999567168dac154716189b9af7
Akul <akurnya@gmail.com>
2015-08-26 21:14:32 +0530

    shift letters generic

---
+++ next_letter_shift.py
@@ -0,0 +1,34 @@
+# Write a procedure, shift, which takes as its input a lowercase letter,
+# a-z and returns the next letter in the alphabet after it, with 'a' 
+# following 'z'.
+
+def shift(letter):
+    return chr(((ord(letter) - 97 + 1) % 26) + 97)
+
+    
+    
+
+
+assert shift('a') == 'b'
+
+assert shift('n') == 'o'
+
+assert shift('z') == 'a'
+
+# Write a procedure, shift_n_letters which takes as its input a lowercase
+# letter, a-z, and an integer n, and returns the letter n steps in the
+# alphabet after it. Note that 'a' follows 'z', and that n can be positive,
+#negative or zero.
+
+def shift_n_letters(letter, n):
+    return chr(((ord(letter) - 97 + n) % 26) + 97)
+
+
+
+assert shift_n_letters('s', 1) == 't'
+
+assert shift_n_letters('s', 2) == 'u'
+
+assert shift_n_letters('s', 10) == 'c'
+
+assert shift_n_letters('s', -10) == 'i'
